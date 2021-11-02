4a69226a0e3eb145fe0e685f2f2afa3c6b0c2b9b
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-11-02 18:49:36 -0300

    updating scripts

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 
 class TestController {
   async test(request: Request, response: Response) {
+    console.log("Received request...");
+
     return response.json({
       timestamp: Number(new Date()),
       message: "Test is just fine!",
